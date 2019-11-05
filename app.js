bdf83c62510ab16c1bb36befbf7f88585ca12f67
abhishekr7 <abhishekrajbhoj38@gmail.com>
2019-11-05 21:33:56 +0530

    pre-final structure

--- app.js
+++ app.js
@@ -3,15 +3,57 @@
 //model -> data
 //view -> send to the user 
 //controller -> grab the data from the "model" and give it to thw view
-var express = require('express');
-var myController = require('./controllers/myController');
-var path = require('path');
+
+//var engines = require('consolidate');
+
+//var express = require('express');
 
 //var myRouter = require('./routes/myRouter');
 
-var app = express();
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const router = express.Router();
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+router.get('/index', function(req,res){
+	res.render('index.html');
+});
+
+router.get('/time', function(req,res){
+	res.render('time_series.html');
+});
+
+router.get('/product', function(req,res){
+	res.render('product_wise.html');
+});
+
+router.get('/country', function(req,res){
+	res.render('country_wise.html');
+});
+
+//add the router
+//Store all HTML files in view folder.
+
+//static files
+//app.use(express.static('./public')); 	// localhost:3000/assets/styles.css
+
+app.use('/', router);
+app.listen(8000);
+
+console.log('port 8000!');
+console.log(__dirname);
+
 
 // firebase app must be listed always and before all firebase products
+
+/*
 var firebase = require("firebase/app");
 
 // firebase products to be used
@@ -70,22 +112,4 @@ firebase.auth().signOut().then(function(){
 
 //************************************************************************
 
-//set up template engine
-//could be html alternatively
-app.set('view engine','ejs');
-
-app.set('views', path.join(__dirname, '/views'));
-
-//static files
-app.use(express.static('./public')); 	// localhost:3000/assets/styles.css
-
-//fire controllers
-myController(app);
-
-//app.use('/', myRouter);
-
-app.listen(8000);
-
-console.log('port 8000!');
-
-module.exports = app;
+*/
