dc026434a9f993556d789e66e405e578f1edaeca
abhishekr7 <abhishekrajbhoj38@gmail.com>
2019-11-08 20:37:13 +0530

    Callbacks added

--- app.js
+++ app.js
@@ -13,42 +13,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+var bodyParser = require('body-parser');
 const router = express.Router();
-
-app.use(express.static(__dirname + '/public'));
-app.set('views', __dirname + '/views');
-
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-router.get('/index', function(req,res){
-	res.render('index.html');
-});
-
-router.get('/time', function(req,res){
-	res.render('time_series.html');
-});
-
-router.get('/product', function(req,res){
-	res.render('product_wise.html');
-});
-
-router.get('/country', function(req,res){
-	res.render('country_wise.html');
-});
-
-//add the router
-//Store all HTML files in view folder.
-
-//static files
-//app.use(express.static('./public')); 	// localhost:3000/assets/styles.css
-
-app.use('/', router);
-app.listen(8000);
-
-console.log('port 8000!');
-console.log(__dirname);
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // firebase app must be listed always and before all firebase products
 var firebase = require("firebase/app");
@@ -59,14 +26,6 @@ require("firebase/firestore");
 require("firebase/functions");
 require("firebase/database");
 
-/*
-var admin = require('firebase-admin');
-var functions = require('firebase-functions');
-*/
-
-//initializing firebase
-
-// firebase config object 
 var firebaseConfig = {
 	// ...
   	apiKey: "AIzaSyAPHwpmhqwAMwJ9iNcsGv_jHywHttOEa6E",
@@ -84,52 +43,66 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-// Attach an asynchronous callback to read the data at our posts reference
+var object ; 
+var f_data = [];
+var f_labels = [];
 
-f_data = [];
-f_labels = [];
+function getData(callback){
+			
+	return database.ref('dates').on("value", function(snapshot) {
+  				
+  	object = snapshot.val();
 
-database.ref('dates').on("value", function(snapshot) {
-  	var object = snapshot.val();
+	var i = 0;
 
-}, function (errorObject) {
- 	console.log("The read failed: " + errorObject.code);
+	for(var key in object){
+
+		f_labels.push(object[key].date);
+		f_data.push(object[key].sales);
+	}
+
+	callback();				
+
+	}, function (errorObject) {	
+ 		console.log("The read failed: " + errorObject.code);
+	});
+		
+}
+
+function renderPageWithData(){
+
+	res.render('time_series.html', {f_labels: f_labels, f_data: f_data});
+}	
+
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+router.get('/index', function(req,res){
+	res.render('index.html'); 
 });
 
-/*
+router.post('/time',urlencodedParser , function(req,res){
 
-// initialize firebase
-firebase.initializeApp(firebaseConfig);
-
-
-//**********************CREATE NEW USER***********************************
-
-firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error){
-	// handle errors here
-	//...
-	var errorCode = error.code;
-	var errorMessage = error.message;
+	getDataFromFirebase(renderPageWithData);
 });
 
-//************************************************************************
-
-//**********************SIGN IN*******************************************
-
-firebase.auth().signInWithEmailAndPassword(email,password).catch(function(error){
-	// handle errors here
-	var errorCode = error.code;
-	var errorMessage = error.message;
+router.get('/product', function(req,res){
+	res.render('product_wise.html');
 });
 
-//************************************************************************
-
-//**********************SIGN OUT*******************************************
-
-firebase.auth().signOut().then(function(){
-	// sign out successful
-}).catch(function(error){
-	// error occurred
+router.get('/country', function(req,res){
+	res.render('country_wise.html');
 });
 
-//************************************************************************
-*/
+app.use('/', router);
+app.listen(8000);
+
+console.log('port 8000!');
+console.log(__dirname);
+
+
+	
